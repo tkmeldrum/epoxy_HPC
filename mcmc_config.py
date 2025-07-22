74ab9aa63bf06d4e70bb598b964adeea9b9cd2e9
Tyler Meldrum <tkmeldrum@wm.edu>
2025-07-22 14:58:07 -0400

    updates before editing for NMR code

--- mcmc_config.py
+++ mcmc_config.py
@@ -4,4 +4,4 @@ burnin = 1000         # number of steps to discard
 stride = 10           # thinning stride for plotting
 overlay_n = 50       # number of posterior samples to overlay
 nwalkers = 16        # number of MCMC walkers
-nsteps = 20000       # total MCMC steps
+nsteps = 8000       # total MCMC steps
